5a82586e441c4a8f932e63f41b3fd8fb6c38445d
Mher Oganesyan <mher_95@hotmail.com>
2018-01-03 16:33:28 -0800

    add first 4

---
+++ 1-indentation.py
@@ -0,0 +1,7 @@
+# Python uses indentation for blocks, instead of curly braces. 
+# Both tabs and spaces are supported, but the standard indentation 
+# requires standard Python code to use four spaces. For example:
+
+x = 1
+if x == 1:
+	print ("x is: %d" % x)
