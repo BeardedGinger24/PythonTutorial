4ae9067940533cf88cd39141d3f37b9c8638c4a1
Mher Oganesyan <mher_95@hotmail.com>
2018-06-11 10:17:45 -0700

    Chapter 2 Practice

--- python_work/Practice-Ch2.py
+++ python_work/Practice-Ch2.py
@@ -99,3 +99,15 @@ print(2 + 4 * 3 / 2)
 famous_number = 24
 print("My favorite number is " + str(famous_number) + ".")
 
+
+'''
+2-10. Adding Comments: Choose two of the programs you’ve written, and
+add at least one comment to each. If you don’t have anything specific to write
+because your programs are too simple at this point, just add your name and
+the current date at the top of each program file. Then write one sentence
+describing what the program does.
+
+2-11. Zen of Python: Enter import this into a Python terminal session and skim
+through the additional principles.
+'''
+
