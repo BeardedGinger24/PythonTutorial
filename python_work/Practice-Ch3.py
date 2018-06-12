48050168712c3edaca2e3f2fa9ae201f872666f7
Mher Oganesyan <mher_95@hotmail.com>
2018-06-12 15:18:38 -0700

    Chapter 3 Practice

--- python_work/Practice-Ch3.py
+++ python_work/Practice-Ch3.py
@@ -218,3 +218,12 @@ print(computer_components)
 computer_components.reverse()
 print(computer_components)
 print(sorted(computer_components, reverse=True))
+
+'''
+3-11. Intentional Error: If you haven’t received an index error in one of your
+programs yet, try to make one happen. Change an index in one of your programs
+to produce an index error. Make sure you correct the error before closing
+the program.
+'''
+
+# 11, print(computer_components[8])
