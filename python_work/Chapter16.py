8c61be2dfc676d04f45db6222dc30c791581c197
Mher Oganesyan <mher_95@hotmail.com>
2018-07-15 10:12:18 -0700

    Chapter 16 Notes

---
+++ python_work/Chapter16.py
@@ -0,0 +1,14 @@
+# Downloading Data
+
+# CSV file format
+# using files:
+# highs_lows.py
+
+# Mapping global data sets: JSON format
+# using files:
+# population_data.json
+# world_population.py
+# countries.py
+# country_codes.py
+# americas.py
+# na_populations.py
