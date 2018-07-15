28636f596e54ba74e58b5ec1a9c201dfb775d994
Mher Oganesyan <mher_95@hotmail.com>
2018-07-15 09:55:58 -0700

    Chapter 15 Notes

---
+++ python_work/Chapter15.py
@@ -0,0 +1,12 @@
+# Generating Data
+# Refer to these files:
+# mpl_squares.py
+# scatter_squares.py
+
+# creating the RandomWlak() Class
+# random_walk.py
+# rw_visual.py
+
+# rolling dice with pygal
+# dice.py
+# die_visual.py
