f9e5e509f8a6c9324dd8a69a8c15d5e04d3ad492
Mher Oganesyan <mher_95@hotmail.com>
2018-07-15 19:21:12 -0700

    Chapter 17 Notes

---
+++ python_work/Chapter17.py
@@ -0,0 +1,19 @@
+# Working with APIs
+'''
+Using a Web API
+
+A web API is a part of a website designed to interact with programs that
+use very specific URLs to request certain information. This kind of request
+is called an API call. The requested data will be returned in an easily processed
+format, such as JSON or CSV. Most apps that rely on external data
+sources, such as apps that integrate with social media sites, rely on API calls.
+'''
+
+# Processing an API Response
+# python_repos.py
+
+# Adding custom tooltips
+# bar_description.py
+
+# The Hacker News API
+# hn_submissions.py
